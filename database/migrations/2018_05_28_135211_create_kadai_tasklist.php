aa68e9b85467feaf4450132c9c2e11c4c64266f4
kaz <ec2-user@ip-172-31-32-44.us-east-2.compute.internal>
2018-05-28 05:27:49 +0000

    message model

---
+++ database/migrations/2018_05_28_135211_create_kadai_tasklist.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKadaiTasklist extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+     
+   /** public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');    // content add column
+            $table->timestamps();
+        });
+    }*/
+    
+    
+       public function up()
+    {
+        Schema::create('tasklists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');    // content add column
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasklists');
+    }
+}
