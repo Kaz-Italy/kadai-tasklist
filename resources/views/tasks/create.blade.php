6a2db861db7188c9199ca1060ce981a6192f92ca
kaz <ec2-user@ip-172-31-32-44.us-east-2.compute.internal>
2018-05-31 03:43:52 +0000

    add another colum

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,8 +9,15 @@
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
         
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        <!--{!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}-->
+
+        <select name="status">
+        <option selected="selected" value="">ステータス</option>
+        <option>未着手</option>
+        <option>実行中</option>
+        <option>完了</option>
+        </select>
 
         {!! Form::submit('追加') !!}
 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -9,12 +9,24 @@
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
         
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        <!--{!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}-->
+
+        <select name="status">
+        <option selected="selected" value="">ステータス</option>
+        <option>未着手</option>
+        <option>実行中</option>
+        <option>完了</option>
+        </select>
 
         {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}
+    
+    
+    
 
 @endsection
+
+
 
